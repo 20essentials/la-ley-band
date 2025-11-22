307e6b077b362067126b92823f9d69a50f8c973c
DominicDev <s0l0estudi4r@gmail.com>
2025-11-22 16:38:48 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LA LEY';
+export const nameSongs = [
+	"EL DUELO", /*1*/
+	"MENTIRA", /*2*/
+	"PRISIONEROS DE LA PIEL", /*3*/
+	"PLACER", /*4*/
+	"EN LUGARES", /*5*/
+	"DESIERTOS", /*6*/
+	"QUE VA A SUCEDER", /*7*/
+	"A VECES", /*8*/
+	"ANGIE", /*9*/
+	"SASHA", /*10*/
+	"SOLO IDEALES", /*11*/
+	"TEJEDORES DE ILUSIÓN", /*12*/
+	"AUTO RUTA", /*13*/
+	"SI TU NO ESTAS AQUI", /*14*/
+	"DECADENCIA", /*15*/
+	"POR UN BINOCULAR", /*16*/
+	"ROCES", /*17*/
+	"I.L.U.", /*18*/
+	"BON VOYAGE", /*19*/
+	"PLEGARIAS", /*20*/
+]
